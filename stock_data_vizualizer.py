1987ae8cf24abccf36e1addb4e876ac899950583
Isaac <isaacrivera117.gmail.com>
2023-10-24 13:56:06 -0500

    updated python file

--- stock_data_vizualizer.py
+++ stock_data_vizualizer.py
@@ -1,1 +1,35 @@
 # A program that uses stock data to produce graphs and charts based on user constraints
+
+def main():
+    run_program = True
+    while run_program == True:
+        print("Hello World")
+        # ^where program should go
+        answer = input("Would you like to view more stock data? (y/n)")
+        if answer == "n":
+            run_program = False
+        else:
+            continue
+        
+main()
+            
+   
+    
+    
+    
+#def get_stock_symbol():
+    #return symbol
+
+#def get_chart_type():
+    #return chart
+    
+#def get_time_series():
+    #return timeSeries
+    
+#def get_start_date():
+    #return startDate
+
+#def get_end_date():
+    #return endDate
+    
+## YYYY-MM-DD
