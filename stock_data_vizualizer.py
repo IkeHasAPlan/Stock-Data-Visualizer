7ed58dc20231bd0ef2cd4cc4cf2b148569385ca2
Erin <eefnmc@umsystem.edu>
2023-10-26 12:25:24 -0500

    added stock symbol function, request module, and implemented in main

--- stock_data_vizualizer.py
+++ stock_data_vizualizer.py
@@ -2,11 +2,18 @@
 
 import matplotlib.pyplot as plt
 from datetime import datetime
+import requests
 
 def main():
     run_program = True
     while run_program == True:
        
+       #stock symbols
+       symbols = get_stock_symbol()
+       if symbols:
+        print("Stock Symbols: ")
+        for symbol in symbols:
+            print(symbol)
        
         # ^where program should go
         answer = input("Would you like to view more stock data? (y/n)")
@@ -15,8 +22,25 @@ def main():
         else:
             continue
         
-#def get_stock_symbol():
-    #return symbol
+def get_stock_symbol():
+    while True:
+        api_key = "IZ2BMG7IZUT81I82"
+        url = f"https://www.alphavantage.co/query?function=LISTING_STATUS&apikey={api_key}"
+
+        try:
+            response = requests.get(url)
+            data = response.json()
+            if "Error Message" in data:
+                print("Error: Unable to retrieve stock symbol.")
+                return None
+            else:
+                symbols = [item['symbol'] for item in data['data']]
+                return symbols
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+            return None
+
+    return symbol
 
 #def get_chart_type():
     #return chart
